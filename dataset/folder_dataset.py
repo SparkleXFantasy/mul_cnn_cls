4573a9947ff11f5f480366014a0fb44af0c38d2b
sxf <1559251498@qq.com>
2023-08-13 11:28:02 +0800

    check file extensions in dataset

--- dataset/folder_dataset.py
+++ dataset/folder_dataset.py
@@ -9,9 +9,10 @@ class FolderDataset(Dataset):
         super(FolderDataset, self).__init__()
         self.data_root = data_root
         self.transform = transform
+        self.supported = ['.png', '.jpg', '.jpeg', '.tif', '.tiff', '.webp']
         self.classes, self.class_to_idx = self.__find_classes(data_root)
         self.images = self.__make_dataset(data_root, self.classes, self.class_to_idx)
-        
+
     def __find_classes(self, dir):
         classes = [d for d in os.listdir(dir) if os.path.isdir(os.path.join(dir, d))]
         classes.sort()
@@ -21,9 +22,10 @@ class FolderDataset(Dataset):
     def __make_dataset(self, dir, classes, class_to_idx):
         images = []
         for cls in classes:
-            for root, _, img_names in os.walk(os.path.join(dir,cls)):
-                for img_name in img_names:
-                    images.append((os.path.join(root, img_name), class_to_idx[cls]))
+            for root, _, names in os.walk(os.path.join(dir,cls)):
+                for name in names:
+                    if os.path.splitext(name)[-1].lower() in self.supported:
+                        images.append((os.path.join(root, name), class_to_idx[cls]))
         return images
         
     def __len__(self):
